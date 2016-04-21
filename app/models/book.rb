11053cab8daca8372c9ac0683a5c5b8a68ce3720
mkrakowiak <krakmis093@gmail.com>
2016-04-21 15:26:26 +0000

    zmiana walidacji

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,5 @@ class Book < ActiveRecord::Base
 	validates :title, length: { minimum: 3 }
 	validates :title, length: { maximum:20 }
     validates :premiere, length: { is:4 }, numericality: true
-    validates :premiere, format: {with: /(12|20)\d{2}/i}  
+    validates :premiere, format: {with: /(12|13|14|15|16|17|18|19|20)\d{2}/i}  
 end
